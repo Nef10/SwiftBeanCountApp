876c27d6aee2415779f0ea23bbb679a27c51168d
Steffen Kötte <steffen.koette@gmail.com>
2017-06-07 00:18:23 -0700

    Initial Commit

---
+++ SwiftBeanCount/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftBeanCount
+//
+//  Created by Steffen Kötte on 2017-06-07.
+//  Copyright © 2017 Steffen Kötte. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwiftBeanCount/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftBeanCount
+//
+//  Created by Steffen Kötte on 2017-06-07.
+//  Copyright © 2017 Steffen Kötte. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
