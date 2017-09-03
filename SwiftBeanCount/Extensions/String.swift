bf3d4773671a5da12d15b4f53d42b40f3c3d54c0
Steffen Kötte <steffen.koette@gmail.com>
2017-09-02 19:02:49 -0700

    Swift update

--- SwiftBeanCount/Extensions/String.swift
+++ SwiftBeanCount/Extensions/String.swift
@@ -15,8 +15,8 @@ extension String {
         let nsString = self as NSString
         let results = regex.matches(in: self, options: [], range: NSRange(self.startIndex..., in: self))
         return results.map { result in
-            (0..<result.numberOfRanges).map { result.rangeAt($0).location != NSNotFound
-                ? nsString.substring(with: result.rangeAt($0))
+            (0..<result.numberOfRanges).map { result.range(at: $0).location != NSNotFound
+                ? nsString.substring(with: result.range(at: $0))
                 : ""
             }
         }
