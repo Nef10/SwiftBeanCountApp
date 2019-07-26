f3e67a9a07ef00f2d09944e64d3fa28e883016ed
Steffen Kötte <steffen.koette@gmail.com>
2019-07-25 23:48:04 -0700

    Open Beancount file and print some statistics

--- SwiftBeanCount/Views/ViewController.swift
+++ SwiftBeanCount/Views/ViewController.swift
@@ -14,6 +14,34 @@ class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        if let dir = FileManager.default.urls(for: .downloadsDirectory, in: .userDomainMask).first {
+            let path = dir.appendingPathComponent("Steffen.beancount")
+            do {
+                let start = Date.timeIntervalSinceReferenceDate
+                let ledger = try Parser.parse(contentOf: path)
+                let end = Date.timeIntervalSinceReferenceDate
+                for error in ledger.errors {
+                    print(error)
+                }
+                for commodity in ledger.commodities {
+                    print(String(describing: commodity))
+                }
+                print(String(format: "Parsing time: %.3f sec", end - start))
+                print("\(ledger.transactions.count) Transactions")
+                print("\(ledger.accounts.count) Accounts")
+                print("\(ledger.accounts.filter { $0.opening != nil }.count) Account openings")
+                print("\(ledger.accounts.filter { $0.closing != nil }.count) Account closings")
+                print("\(ledger.tags.count) Tags")
+                print("\(ledger.commodities.count) Commodities")
+                print("\(ledger.errors.count) Errors")
+
+                print(String(describing: Array(Set(ledger.transactions.map { $0.metaData.payee })).filter { !$0.isEmpty }.sorted { $0.lowercased() < $1.lowercased() }))
+            } catch let error {
+                print(error)
+            }
+        }
+
     }
 
 }
