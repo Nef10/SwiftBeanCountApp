56ce0620f1021767bc9618d9d5fe9e1f731e87d1
Steffen Kötte <steffen.koette@gmail.com>
2025-12-17 11:27:37 +0100

    Update ImporterHelpView for iOS

--- SwiftBeanCountApp/Tabs/Importer/ImporterHelpView.swift
+++ SwiftBeanCountApp/Tabs/Importer/ImporterHelpView.swift
@@ -9,35 +9,82 @@ import SwiftBeanCountImporter
 import SwiftUI
 
 struct HelpTextView: View {
+
+    let title: String
     let content: String
+
+#if !os(macOS)
+    let dismiss: DismissAction?
+#endif
 
     var body: some View {
         ScrollView {
             Text(content).padding()
-        }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+        .navigationTitle(title)
+#if !os(macOS)
+        .toolbar {
+            Button(role: .close) {
+                dismiss?()
+            }
+        }
+#endif
     }
 }
 
 struct ImporterHelpView: View {
+
+#if !os(macOS)
+    @Environment(\.dismiss)
+    var dismiss
+#endif
+
     var body: some View {
-        NavigationView {
-            List {
-                Section(header: Text("Importers")) {
-                    ForEach(0..<ImporterFactory.allImporters.count, id: \.self) { index in
-                        NavigationLink(destination: HelpTextView(content: ImporterFactory.allImporters[index].helpText)) {
-                            Text(ImporterFactory.allImporters[index].importerName)
-                        }
-                    }
-                }
-            }.listStyle(SidebarListStyle())
+#if os(macOS)
+        NavigationSplitView {
+            list
+        } detail: {
             Text("Select an importer to see its help text.").frame(maxWidth: .infinity, maxHeight: .infinity)
-        }.frame(minWidth: 500, idealWidth: 700, maxWidth: .infinity, minHeight: 300, idealHeight: 500, maxHeight: .infinity, alignment: .center)
+        }
+        .frame(minWidth: 500, idealWidth: 700, maxWidth: .infinity, minHeight: 300, idealHeight: 500, maxHeight: .infinity, alignment: .center)
+#else
+        NavigationStack {
+            list.toolbar {
+                Button(role: .close) {
+                    dismiss()
+                }
+            }
+        }
+#endif
+    }
+
+    var list: some View {
+        List(ImporterFactory.allImporters.indices, id: \.self) { index in
+            NavigationLink(ImporterFactory.allImporters[index].importerName, value: index)
+        }
+        .navigationDestination(for: Int.self) { index in
+#if os(macOS)
+            HelpTextView(title: ImporterFactory.allImporters[index].importerName,
+                         content: ImporterFactory.allImporters[index].helpText)
+#else
+            HelpTextView(title: ImporterFactory.allImporters[index].importerName,
+                         content: ImporterFactory.allImporters[index].helpText,
+                         dismiss: dismiss)
+#endif
+        }
+        .listStyle(SidebarListStyle())
+        .navigationTitle("Importer Help")
     }
 }
 
 struct HelpTextView_Previews: PreviewProvider {
     static var previews: some View {
-        HelpTextView(content: "ABC\ndef\n\ngh").previewLayout(.sizeThatFits)
+#if os(macOS)
+        HelpTextView(title: "Title", content: "ABC\ndef\n\ngh").previewLayout(.sizeThatFits)
+#else
+        HelpTextView(title: "Title", content: "ABC\ndef\n\ngh", dismiss: nil).previewLayout(.sizeThatFits)
+#endif
     }
 }
 
