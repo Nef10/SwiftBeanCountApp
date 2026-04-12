c27aa7fb3ab56a90b5a682cba317f20de7624705
Steffen Kötte <steffen.koette@gmail.com>
2026-04-11 21:54:28 -0700

    Tweak platform checks (#88)

--- SwiftBeanCountApp/SwiftBeanCountApp.swift
+++ SwiftBeanCountApp/SwiftBeanCountApp.swift
@@ -65,8 +65,7 @@ struct SwiftBeanCountApp: App {
         tabs += [
             Tab(title: "Statements", icon: "doc.text", view: AnyView(Statements())),
         ]
-#endif
-#if os(iOS)
+#else
         tabs += [
             Tab(title: "Ledger", icon: "doc.fill", view: AnyView(ChangeLedgerView())),
             Tab(title: "Settings", icon: "gear", view: AnyView(SettingsView())),
